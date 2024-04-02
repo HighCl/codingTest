f7abf7f4a572f2a51bd7fab44b93f72a9e959725
SungWook Kang <64746739+HighCl@users.noreply.github.com>
2024-04-02 18:43:42 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stack>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <list>
+#include <set>
+#include <sstream> // 이제 프로그램 내에서 stringstream을 사용할 수 있습니다.
+using namespace std;
+
+void Logic(const char* str)
+{
+}
+
+void Input()
+{
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	string str;
+
+	cin >> str;
+
+	//기본1
+	cout << str.length();
+}
