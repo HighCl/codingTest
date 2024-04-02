0e101e80e6b230ece61485ab87aa5434ddc9c67a
SungWook Kang <64746739+HighCl@users.noreply.github.com>
2024-04-02 18:56:14 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stack>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <list>
+#include <set>
+using namespace std;
+
+void Logic()
+{
+}
+
+void Input()
+{
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	string str;
+	cin >> str;
+	cout << (int)str[0] << endl;
+}
