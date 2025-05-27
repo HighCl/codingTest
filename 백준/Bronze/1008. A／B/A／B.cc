c608899e183eea07b24614f99a005f6eb2cdec8b
HighCl <64746739+HighCl@users.noreply.github.com>
2025-05-28 01:14:22 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	cout.precision(10);
+	cout << (double)n / m;
+	return 0;
+}
